4ed3b50f76a512971ed2a7acc23729dc7c45a653
Oliver Holmes <oliver.albert.holmes@gmail.com>
2023-04-30 20:26:33 +0930

    Prep for deployment

--- src/pages/CodeOfConduct.tsx
+++ src/pages/CodeOfConduct.tsx
@@ -15,7 +15,15 @@ const CodeOfConductPage = () => {
 	})
 
     return (
-        <div >
+      <>
+      <section 
+        style={{ 
+          backgroundImage: "url('/imgs/office_with_paper_work.png')", 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center -650px' 
+        }}
+        className="relative flex items-center justify-center h-64 bg-gray-100 bg-no-repeat bg-cover bg-center"
+      ></section>
         <section className="container px-6 py-8 mx-auto lg:py-16">
           <div>
           <Markdown
@@ -23,13 +31,23 @@ const CodeOfConductPage = () => {
                 overrides: {
                     h1: {
                       props: {
-                          className: 'text-2xl font-semibold text-gray-700 capitalize',
+                          className: 'text-3xl font-medium text-gray-800 md:text-2xl lg:text-3xl',
                       },
                     },
                     h2: {
                       props: {
+                          className: 'text-2xl font-semibold text-gray-700 capitalize',
+                      },
+                    },
+                    h3: {
+                      props: {
                           className: 'text-xl font-semibold text-gray-700 capitalize',
                       },
+                    },
+                    ul: { 
+                      props: {
+                        className: 'list-disc list-inside',
+                      }
                     }
                 },
             }}
@@ -38,7 +56,7 @@ const CodeOfConductPage = () => {
         </div>
         </section>
 
-        </div>
+        </>
     );
 };
 export default CodeOfConductPage;
