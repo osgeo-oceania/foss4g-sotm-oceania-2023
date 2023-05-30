60719c335e052de5bef483aa9f427c0ab4fe3323
Oliver Holmes <oliver.albert.holmes@gmail.com>
2023-05-30 21:53:29 +0930

    Add noreferrer to button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ type ButtonProps = {
 const Button: React.FC<ButtonProps> = ({ children, target }) => {
   return (
     <button className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-        <a href={target} target='_blank'>{children}</a>
+        <a href={target} target='_blank' rel="noreferrer">{children}</a>
     </button>
     // <div className="flex w-full max-w-sm overflow-hidden rounded-lg shadow-md bg-gray-800">
     //     <div className="w-2 bg-gray-900"></div>
