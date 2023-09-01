a30d27c2d4216b68baf1c45c0d9d350aa8852e37
Emma Hain <emmahain@gmail.com>
2023-09-01 17:40:08 +1000

    Update KeynoteSpeakers.tsx

--- src/pages/KeynoteSpeakers.tsx
+++ src/pages/KeynoteSpeakers.tsx
@@ -17,10 +17,10 @@ const PresentPage = () => {
         </h3>
         <div className="md:flex md:flex-row justify-around">
           <SpeakerCard
-          <!--  imgSrc="./imgs/keynote-speakers/CarrolChan.jpg"
+           imgSrc="./imgs/keynote-speakers/CarrolChan.jpg"
             title="Carrol Chan"
             shortDescription="Building and progressing a mapping community in the Pacific, what is the role of unseen bias?"
-            longDescriptionSrc="./documents/keynote-speakers/carrol-chan.md"-->
+            longDescriptionSrc="./documents/keynote-speakers/carrol-chan.md"
           />
           <SpeakerCard
             imgSrc="./imgs/keynote-speakers/JeremyPalmer.jpg"
