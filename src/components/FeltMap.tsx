c7466a210d84c29be1d3c4ffcf50148ed72ed7e0
Emma Hain <emmahain@gmail.com>
2023-09-15 21:24:54 +1000

    Create FeltMap.tsx

---
+++ src/components/FeltMap.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const FeltMap = () => {
+    return React.createElement(
+      'felt-map',
+      {
+<iframe 
+  width="100%" 
+  height="450" 
+  frameborder="0" 
+  title="Felt Map" 
+  src="https://felt.com/embed/map/FOSS4G-SoTM-Oceania-2023-uh5Q09CxnReyLIfjfsacYXD?loc=-36.84446,174.78018,13.95z">
+</iframe>
+      }
+    );
+  };
+  
+  export default FeltMap;
