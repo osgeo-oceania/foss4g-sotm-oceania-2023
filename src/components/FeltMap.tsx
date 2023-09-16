fe6d75593b29aab0fb1f8dbc9a9b11095964531c
Emma Hain <emmahain@gmail.com>
2023-09-16 13:12:07 +1000

    Update FeltMap.tsx

--- src/components/FeltMap.tsx
+++ src/components/FeltMap.tsx
@@ -3,7 +3,7 @@ const FeltMap = () => {
   return (
     <iframe
       title="Felt Map"
-      src="https://felt.com/embed/map/FOSS4G-SoTM-Oceania-2023-uh5Q09CxnReyLIfjfsacYXD?loc=-36.84446,174.78018,13.95z"
+      src="https://felt.com/embed/map/FOSS4G-SoTM-Oceania-2023-uh5Q09CxnReyLIfjfsacYXD?loc=-36.84724,174.76043,15.02z">
       style={{ width: "100%", minHeight: "100vh" }}
       sandbox="allow-scripts allow-same-origin allow-popups"
     />
