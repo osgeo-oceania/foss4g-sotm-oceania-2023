ddede7955cd9913b4c41d56f3a924205957489f0
Oliver Holmes <oliver.albert.holmes@gmail.com>
2023-09-22 12:03:07 +0930

    Update calender to universal display

--- src/components/DayView.tsx
+++ src/components/DayView.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type Event = {
   id: number;
   start: string; // HH:mm
   duration: string; // HH:mm
   title: string;
+  date?: string;
   url?: string;
   persons?: { public_name: string }[];
+};
+
+type EventsByRoomAndDate = {
+  [roomName: string]: {
+    [date: string]: Event;
+  };
 };
 
 export type Day = {
@@ -54,30 +61,155 @@ const handleClick = (url: string) => {
 
 const DayView: React.FC<DayViewProps> = ({ day, dayConfig }) => {
   const { periods, specialEvents = [] } = dayConfig;
-  // Generate time slots for each period
-  const timeSlots: { time: number; interval: number }[] = periods.flatMap(
-    ({ startTime, endTime, timeSlotInterval }) => {
-      const totalMinutes = (endTime - startTime) * 60;
-      return Array.from(
-        { length: totalMinutes / timeSlotInterval },
-        (_, i) => ({
-          time: startTime * 60 + i * timeSlotInterval,
-          interval: timeSlotInterval,
-        })
-      );
-    }
-  );
+  const [timeSlots, setTimeSlots] = useState<
+    { time: number; interval: number }[]
+  >([]);
+  const [roomsWithEvents, setRooms] = useState<EventsByRoomAndDate>({});
 
-  // Merge specialEvents with room events
-  const roomsWithSpecialEvents = { ...day.rooms };
-  for (const roomName in roomsWithSpecialEvents) {
-    roomsWithSpecialEvents[roomName] = [
-      ...roomsWithSpecialEvents[roomName],
-      ...specialEvents, // Add the special events here
-    ];
-  }
+  useEffect(() => {
+    // Generate time slots for each period
+    const timeSlotsCal: { time: number; interval: number }[] = periods.flatMap(
+      ({ startTime, endTime, timeSlotInterval }) => {
+        const totalMinutes = (endTime - startTime) * 60;
+        return Array.from(
+          { length: totalMinutes / timeSlotInterval },
+          (_, i) => ({
+            time: startTime * 60 + i * timeSlotInterval,
+            interval: timeSlotInterval,
+          })
+        );
+      }
+    );
+
+    setTimeSlots(timeSlotsCal);
+
+    // Merge specialEvents with room events
+    const roomsWithSpecialEvents = { ...day.rooms };
+    for (const roomName in roomsWithSpecialEvents) {
+      roomsWithSpecialEvents[roomName] = [
+        ...roomsWithSpecialEvents[roomName],
+        ...specialEvents, // Add the special events here
+      ];
+    }
+    
+    let eventsByRoomAndDate: EventsByRoomAndDate = {};
+
+    Object.keys(roomsWithSpecialEvents).forEach((roomName) => {
+      roomsWithSpecialEvents[roomName].forEach((event) => {
+        let time = '';
+
+        if (event.date !== undefined) {
+          time = event.date.split("T")[1];
+        }
+
+        if (event.start !== null) {
+          time = event.start;
+        }
+
+        // Extract hours and minutes from the string
+        const [hours, minutes] = time.split(":").map(Number);
+
+        // Determine AM or PM
+        const isPm = hours >= 12;
+        const adjustedHours = hours % 12 || 12; // Convert 0 to 12 for 12AM
+
+        // Format the hours and minutes with AM/PM
+        const amPmTime = `${adjustedHours}:${String(minutes).padStart(2, '0')} ${isPm ? 'PM' : 'AM'}`;
+        
+        if (!eventsByRoomAndDate[roomName]) {
+          eventsByRoomAndDate[roomName] = {};
+        }
+  
+        if (!eventsByRoomAndDate[roomName][amPmTime]) {
+          eventsByRoomAndDate[roomName][amPmTime] = event;
+        }
+      });
+    });
+
+    setRooms(eventsByRoomAndDate);
+
+  }, [day, periods, specialEvents]);
+
+
+  const renderEventDetails = (event: any, interval:any, time: any) => {
+    const startMinutes = timeToMinutes(event.start);
+    const durationMinutes = timeToMinutes(event.duration);
+    const height = (durationMinutes / interval) * 50;
+  
+    if (time !== startMinutes) return null;
+  
+    const color = 
+      event.title === "Lunch"
+        ? "bg-green-200"
+        : event.title.includes("Tea")
+        ? "bg-yellow-200"
+        : "bg-blue-200";
+  
+    const hoverColor =
+      event.title === "Lunch"
+        ? "bg-green-200"
+        : event.title.includes("Tea")
+        ? "bg-yellow-200"
+        : "bg-red-300";
+  
+    const EventDiv = (
+      <div
+        key={event.id}
+        className={`${color} p-1 rounded absolute hover:${hoverColor}`}
+        style={{
+          top: `0px`,
+          height: `${height - 6}px`,
+          width: "95%",
+          zIndex: 10,
+          userSelect: "none",
+          marginTop: "3px",
+        }}
+      >
+        {event.id > 0 ? (
+          <>
+            <div className="text-sm text-white bg-blue-400 p-1 rounded">
+              <strong>
+                Start: {toAmPm(time, interval)} <br />
+                Duration: {toHours(event.duration)}
+              </strong>
+            </div>
+            <strong className="text-xs lg:text-sm xl:text-lg">
+              {event.title}
+            </strong>
+            <div className="absolute text-xs lg:text-sm bottom-0 p-3">
+              Presenter:
+              <br />
+              <strong>
+                {event.persons && event.persons.length > 0
+                  ? event.persons.map((person: any) => person.public_name).join(", ")
+                  : ""}
+              </strong>
+            </div>
+          </>
+        ) : (
+          <strong className="text-sm">{event.title}</strong>
+        )}
+      </div>
+    );
+          
+    return event.url ? (
+      <div
+        onClick={() => handleClick(event.url ? event.url : "")}
+        key={event.id}
+        style={{
+          cursor: "pointer",
+          zIndex: 10,
+        }}
+      >
+        {EventDiv}
+      </div>
+    ) : (
+      EventDiv
+    );
+  };
 
   return (
+    <>
     <table className="min-w-full border-collapse">
       <thead>
         <tr className="h-[50px]">
@@ -95,92 +227,22 @@ const DayView: React.FC<DayViewProps> = ({ day, dayConfig }) => {
             <td className="border p-2 text-right pr-4">
               {toAmPm(time, interval)}
             </td>
-            {Object.keys(roomsWithSpecialEvents).map((roomName, index) => (
+            {Object.keys(roomsWithEvents).map((roomName, index) => (
               <td key={index} className="border p-2 relative">
-                {roomsWithSpecialEvents[roomName].map((event) => {
-                  const startMinutes = timeToMinutes(event.start);
-                  const durationMinutes = timeToMinutes(event.duration);
-                  const height = (durationMinutes / interval) * 50;
-
-                  if (time === startMinutes) {
-                    const color =
-                      event.title === "Lunch"
-                        ? "bg-green-200"
-                        : event.title.includes("Tea")
-                        ? "bg-yellow-200"
-                        : "bg-blue-200";
-
-                    const hoverColor =
-                      event.title === "Lunch"
-                        ? "bg-green-200"
-                        : event.title.includes("Tea")
-                        ? "bg-yellow-200"
-                        : "bg-red-300";
-
-                    const EventDiv = (
-                      <div
-                        key={event.id}
-                        className={`${color} p-1 rounded absolute hover:${hoverColor}`}
-                        style={{
-                          top: `0px`,
-                          height: `${height}px`,
-                          width: "95%",
-                          zIndex: 10,
-                          userSelect: "none",
-                        }}
-                      >
-                        {event.id > 0 ? (
-                          <>
-                            <div className="text-sm text-white bg-blue-400 p-1 rounded">
-                              <strong>
-                                Start: {toAmPm(time, interval)} <br />
-                                Duration: {toHours(event.duration)}
-                              </strong>
-                            </div>
-                            <strong className="text-xs lg:text-sm xl:text-lg">
-                              {event.title}
-                            </strong>
-                            <div className="absolute text-xs lg:text-sm bottom-0 p-3">
-                              Presenter:
-                              <br />
-                              <strong>
-                                {event.persons && event.persons.length > 0
-                                  ? event.persons
-                                      .map((person) => person.public_name)
-                                      .join(", ")
-                                  : ""}
-                              </strong>
-                            </div>
-                          </>
-                        ) : (
-                          <strong className="text-sm">{event.title}</strong>
-                        )}
-                      </div>
-                    );
-
-                    return event.url ? (
-                      <div
-                        onClick={() => handleClick(event.url ? event.url : "")}
-                        key={event.id}
-                        style={{
-                          cursor: "pointer",
-                          zIndex: 10,
-                        }}
-                      >
-                        {EventDiv}
-                      </div>
-                    ) : (
-                      EventDiv
-                    );
-                  }
-                  return null;
-                })}
-              </td>
+              {roomsWithEvents && roomsWithEvents[roomName] && roomsWithEvents[roomName][toAmPm(time, interval)] && renderEventDetails(roomsWithEvents[roomName][toAmPm(time, interval)], interval, time)}
+            </td>
             ))}
           </tr>
         ))}
       </tbody>
     </table>
+    <div>
+      Total events in eventsByRoomAndDate: {Object.values(roomsWithEvents).reduce(
+  (acc, roomEvents) => acc + Object.keys(roomEvents).length,
+  0
+)}
+    </div>
+    </>
   );
 };
 
--- src/pages/Program/Program.tsx
+++ src/pages/Program/Program.tsx
@@ -1,111 +1,116 @@
-import DayView from "../../components/DayView";
+// import DayView from "../../components/DayView";
 import DayViewSmall from "../../components/DayViewSmall";
-import { Day } from "../../components/DayView";
+// import { Day } from "../../components/DayView";
 
 import { useEffect, useState } from "react";
 
 // Define a function to get special events based on the date
-const getDayConfig = (day: Day) => {
-  switch (day.date) {
-    case "2023-10-16":
-      return {
-        periods: [{ startTime: 7, endTime: 18, timeSlotInterval: 30 }],
-        specialEvents: [
-          { id: -1, start: "12:00", duration: "01:00", title: "Lunch" },
-          {
-            id: -2,
-            start: "10:00",
-            duration: "00:15",
-            title: "Morning Tea",
-          },
-          {
-            id: -3,
-            start: "15:00",
-            duration: "00:15",
-            title: "Afternoon Tea",
-          },
-        ],
-      };
-    case "2023-10-17":
-      return {
-        periods: [
-          { startTime: 8.5, endTime: 11, timeSlotInterval: 15 },
-          { startTime: 11, endTime: 12, timeSlotInterval: 5 },
-          { startTime: 12, endTime: 16.5, timeSlotInterval: 5 },
-          { startTime: 16.5, endTime: 17.35, timeSlotInterval: 1 },
-        ],
-        specialEvents: [
-          { id: -1, start: "12:15", duration: "01:00", title: "Lunch" },
-          {
-            id: -2,
-            start: "10:30",
-            duration: "00:30",
-            title: "Morning Tea",
-          },
-          {
-            id: -3,
-            start: "15:00",
-            duration: "00:30",
-            title: "Afternoon Tea",
-          },
-        ],
-      };
-    case "2023-10-18":
-      return {
-        periods: [
-          { startTime: 10, endTime: 16.75, timeSlotInterval: 5 },
-          { startTime: 16.75, endTime: 17.35, timeSlotInterval: 1 },
-        ],
-        specialEvents: [
-          { id: -1, start: "12:50", duration: "01:00", title: "Lunch" },
-          {
-            id: -2,
-            start: "10:40",
-            duration: "00:30",
-            title: "Morning Tea",
-          },
-          {
-            id: -3,
-            start: "15:05",
-            duration: "00:30",
-            title: "Afternoon Tea",
-          },
-        ],
-      };
-    case "2023-10-19":
-      return {
-        periods: [{ startTime: 11.75, endTime: 18, timeSlotInterval: 5 }],
-        specialEvents: [
-          { id: -1, start: "12:35", duration: "01:00", title: "Lunch" },
-          {
-            id: -2,
-            start: "10:50",
-            duration: "00:30",
-            title: "Morning Tea",
-          },
-          {
-            id: -3,
-            start: "14:25",
-            duration: "00:30",
-            title: "Afternoon Tea",
-          },
-        ],
-      };
-    default:
-      return {
-        periods: [{ startTime: 8, endTime: 14, timeSlotInterval: 60 }],
-        specialEvents: [],
-      };
-  }
-};
+// const getDayConfig = (day: Day) => {
+//   switch (day.date) {
+//     case "2023-10-16":
+//       return {
+//         periods: [{ startTime: 7, endTime: 18, timeSlotInterval: 15 }],
+//         specialEvents: [
+//           { id: -1, start: "12:00", duration: "01:00", title: "Lunch" },
+//           {
+//             id: -2,
+//             start: "10:00",
+//             duration: "00:15",
+//             title: "Morning Tea",
+//           },
+//           {
+//             id: -3,
+//             start: "15:00",
+//             duration: "00:15",
+//             title: "Afternoon Tea",
+//           },
+//         ],
+//       };
+//     case "2023-10-17":
+//       return {
+//         periods: [
+//           { startTime: 8.5, endTime: 11, timeSlotInterval: 10 },
+//           { startTime: 11, endTime: 12, timeSlotInterval: 5 },
+//           { startTime: 12, endTime: 16.5, timeSlotInterval: 5 },
+//           { startTime: 16.5, endTime: 17.35, timeSlotInterval: 1 },
+//         ],
+//         specialEvents: [
+//           { id: -1, start: "12:15", duration: "01:00", title: "Lunch" },
+//           {
+//             id: -2,
+//             start: "10:30",
+//             duration: "00:30",
+//             title: "Morning Tea",
+//           },
+//           {
+//             id: -3,
+//             start: "15:00",
+//             duration: "00:30",
+//             title: "Afternoon Tea",
+//           },
+//         ],
+//       };
+//     case "2023-10-18":
+//       return {
+//         periods: [
+//           { startTime: 10, endTime: 16.75, timeSlotInterval: 5 },
+//           { startTime: 16.75, endTime: 17.35, timeSlotInterval: 1 },
+//         ],
+//         specialEvents: [
+//           { id: -1, start: "12:50", duration: "01:00", title: "Lunch" },
+//           {
+//             id: -2,
+//             start: "10:40",
+//             duration: "00:30",
+//             title: "Morning Tea",
+//           },
+//           {
+//             id: -3,
+//             start: "15:05",
+//             duration: "00:30",
+//             title: "Afternoon Tea",
+//           },
+//         ],
+//       };
+//     case "2023-10-19":
+//       return {
+//         periods: [{ startTime: 11.75, endTime: 18, timeSlotInterval: 5 }],
+//         specialEvents: [
+//           { id: -1, start: "12:35", duration: "01:00", title: "Lunch" },
+//           {
+//             id: -2,
+//             start: "10:50",
+//             duration: "00:30",
+//             title: "Morning Tea",
+//           },
+//           {
+//             id: -3,
+//             start: "14:25",
+//             duration: "00:30",
+//             title: "Afternoon Tea",
+//           },
+//         ],
+//       };
+//     default:
+//       return {
+//         periods: [{ startTime: 8, endTime: 14, timeSlotInterval: 60 }],
+//         specialEvents: [],
+//       };
+//   }
+// };
 
 const formatDate = (dateString: string) => {
+  const [year, month, day] = dateString.split('-').map(Number);
+  const date = new Date(Date.UTC(year, month - 1, day)); // month is 0-indexed
+  
   const options: Intl.DateTimeFormatOptions = {
     weekday: "short",
     month: "short",
     day: "numeric",
+    timeZone: "UTC"
   };
-  return new Date(dateString).toLocaleDateString(undefined, options);
+
+  return new Intl.DateTimeFormat(undefined, options).format(date);
 };
 
 const ProgramPage = () => {
@@ -155,20 +160,11 @@ const ProgramPage = () => {
             </button>
           ))}
         </div>
-      </section>
-      <div className="hidden lg:block">
-        {allDaysData[activeDay] && (
-          <DayView
-            day={allDaysData[activeDay]}
-            dayConfig={getDayConfig(allDaysData[activeDay])}
-          />
-        )}
-      </div>
-      <div className="block lg:hidden">
         {allDaysData[activeDay] && (
           <DayViewSmall day={allDaysData[activeDay]} />
         )}
-      </div>
+      </section>
+      
     </>
   );
 };
