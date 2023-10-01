0412132f5733084cf79528085fa95f0a66736df0
Emma Hain <emmahain@gmail.com>
2023-10-01 11:38:33 +1000

    Update ProductShowcase.tsx

--- src/pages/Program/ProductShowcase.tsx
+++ src/pages/Program/ProductShowcase.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import {Outlet} from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
